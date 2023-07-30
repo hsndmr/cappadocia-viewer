c1028143c4bb2c92d8a197d708520dd7d93eeea3
Hasan Demir <hsndmr5044@hotmail.com>
2023-07-30 04:38:15 +0300

    test: update App test to include mocks and async rendering

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,23 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import App from '../renderer/App';
 
+jest.mock('@orama/orama', () => ({
+  create: jest.fn().mockResolvedValue(jest.fn()),
+  insert: jest.fn().mockResolvedValue(jest.fn()),
+}));
+
+jest.mock('socket.io-client', () => ({
+  io: jest.fn(() => ({
+    on: jest.fn(),
+    off: jest.fn(),
+  })),
+}));
+
 describe('App', () => {
-  it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
+  it('should render', async () => {
+    await waitFor(() => {
+      expect(render(<App />)).toBeTruthy();
+    });
   });
 });
