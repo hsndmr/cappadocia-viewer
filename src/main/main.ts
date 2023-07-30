b2828a0e0d2d50f0858bbe48f196a85e878e4837
Hasan Demir <hsndmr5044@hotmail.com>
2023-07-30 04:04:03 +0300

    refactor: remove unused IPC example code

--- src/main/main.ts
+++ src/main/main.ts
@@ -9,7 +9,7 @@
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
 import path from 'path';
-import { app, BrowserWindow, shell, ipcMain } from 'electron';
+import { app, BrowserWindow, shell } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
@@ -27,12 +27,6 @@ class AppUpdater {
 let mainWindow: BrowserWindow | null = null;
 
 const server = createServer();
-
-ipcMain.on('ipc-example', async (event, arg) => {
-  const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
-  console.log(msgTemplate(arg));
-  event.reply('ipc-example', msgTemplate('pong'));
-});
 
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
