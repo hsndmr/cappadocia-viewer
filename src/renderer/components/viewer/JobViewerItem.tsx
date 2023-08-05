3aaf2307ceced9e71e775af6df2bd0de1a13ec9e
Hasan Demir <hsndmr5044@hotmail.com>
2023-08-05 14:13:04 +0300

    feat: add new JobViewerItem component for job-related messages

---
+++ src/renderer/components/viewer/JobViewerItem.tsx
@@ -0,0 +1,17 @@
+import Box from '@mui/material/Box';
+import { Stack } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import { BaseViewerItemProps } from './ViewerItem';
+
+export default function JobViewerItem({ viewer }: BaseViewerItemProps) {
+  return (
+    <Stack spacing={1}>
+      <Box
+        sx={{
+          flexShrink: 1,
+        }}
+      />
+      <Typography>{viewer.message}</Typography>
+    </Stack>
+  );
+}
