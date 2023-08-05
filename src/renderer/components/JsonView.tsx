b391aebe8f3a6f4543e2689a4866c685360c5ba6
Hasan Demir <hsndmr5044@hotmail.com>
2023-08-05 20:03:34 +0300

    feat: add font size

--- src/renderer/components/JsonView.tsx
+++ src/renderer/components/JsonView.tsx
@@ -1,34 +1,43 @@
+import styled from '@emotion/styled';
+import { Box } from '@mui/material';
 import { JSONTree } from 'react-json-tree';
 
+const StyledBox = styled(Box)(() => ({
+  '& ul': {
+    fontSize: 16,
+  },
+}));
 interface JsonViewProps {
   data: unknown;
 }
 
 export default function JsonView({ data }: JsonViewProps) {
   return (
-    <JSONTree
-      theme={{
-        scheme: 'monokai',
-        author: 'wimer hazenberg (http://www.monokai.nl)',
-        base00: '#272822',
-        base01: '#383830',
-        base02: '#49483e',
-        base03: '#75715e',
-        base04: '#a59f85',
-        base05: '#f8f8f2',
-        base06: '#f5f4f1',
-        base07: '#f9f8f5',
-        base08: '#f92672',
-        base09: '#fd971f',
-        base0A: '#f4bf75',
-        base0B: '#a6e22e',
-        base0C: '#a1efe4',
-        base0D: '#66d9ef',
-        base0E: '#ae81ff',
-        base0F: '#cc6633',
-      }}
-      hideRoot
-      data={data}
-    />
+    <StyledBox>
+      <JSONTree
+        theme={{
+          scheme: 'monokai',
+          author: 'wimer hazenberg (http://www.monokai.nl)',
+          base00: '#272822',
+          base01: '#383830',
+          base02: '#49483e',
+          base03: '#75715e',
+          base04: '#a59f85',
+          base05: '#f8f8f2',
+          base06: '#f5f4f1',
+          base07: '#f9f8f5',
+          base08: '#f92672',
+          base09: '#fd971f',
+          base0A: '#f4bf75',
+          base0B: '#a6e22e',
+          base0C: '#a1efe4',
+          base0D: '#66d9ef',
+          base0E: '#ae81ff',
+          base0F: '#cc6633',
+        }}
+        hideRoot
+        data={data}
+      />
+    </StyledBox>
   );
 }
